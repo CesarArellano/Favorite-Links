bf42b621d16db745c3ba7793537aecdc09d77236
CesarArellano <raywayday@gmail.com>
2020-08-03 17:24:38 -0500

    Saving Data in MySQL

--- src/routes/links.js
+++ src/routes/links.js
@@ -8,7 +8,14 @@ router.get('/add', (req,res) => {
 });
 
 router.post('/add', (req,res) => {
-  res.send('received');
+  const { title, url, description } = req.body;
+  const newLink = {
+    title,
+    url,
+    description
+  };
+  pool.query('INSERT INTO links set ?',[newLink]) /* Petición asincrona, con await le decimos que va a tomar su tiempo, cuando se ejecute siga con el flujo del programa. */
+    .then(() => { res.send('received');})
 });
 
 module.exports = router;
